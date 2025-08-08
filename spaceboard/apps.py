1f3081e1125ad2f55735290a03b0dae27a39a71b
Jana Möllenbeck <janamllenbeck@gmail.com>
2025-08-08 14:28:22 +0200

    feat: add postgresql db

--- spaceboard/apps.py
+++ spaceboard/apps.py
@@ -5,7 +5,3 @@ from django.urls import reverse
 class SpaceboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'spaceboard'
-
-    #def ready(self):
-     #   from django.contrib import admin
-      #  admin.site.site_url = reverse('spaceboard:dashboard')
