83bf76a662a63f18bd22ac39fbcb7b15a2233eac
Jana Möllenbeck <janamllenbeck@gmail.com>
2025-08-08 13:01:52 +0200

    fix: SpaceboardConfig

--- spaceboard/apps.py
+++ spaceboard/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from django.urls import reverse
 
 
 class SpaceboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'spaceboard'
+
+    def ready(self):
+        from django.contrib import admin
+        admin.site.site_url = reverse('spaceboard:dashboard')
