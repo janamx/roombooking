4be758d2e0017813d8294514822b3ab958ab10fb
Jana Möllenbeck <janamllenbeck@gmail.com>
2025-08-09 14:25:57 +0200

    feat: add gitignore and distinction between postgres and local sqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,19 +78,31 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
-}
+if os.environ.get("RENDER"):
+    # Postgres auf Render
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    # SQLite lokal
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 #DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-    #}
+ #   
+  #  'default': dj_database_url.config(
+   #     default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    #)
 #}
+
 
 
 # Password validation
